6a6940ccf7b56b942c8d1d171a0ea6f1a1016ebd
Danila Molodtsov <danmol04@gmail.com>
2020-05-15 01:25:59 +0300

    real init commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.scss';
+import "antd/dist/antd.css";
+import '@csstools/normalize.css';
+
+function App() {
+  return (
+    <div className="App">
+      <p>some app</p>
+    </div>
+  );
+}
+
+export default App;
